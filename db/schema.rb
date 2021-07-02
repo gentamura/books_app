2b786f9b19057b687b9bffdb377f01f866fbedff
Gen Tamura <gen.tamura84@gmail.com>
2021-07-02 18:22:16 +0900

    Add author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_091529) do
+ActiveRecord::Schema.define(version: 2021_07_02_091907) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
