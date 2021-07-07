1b7af3b50f1f8484fb9ed1d8fd278bb100196154
Gen Tamura <gen.tamura84@gmail.com>
2021-07-07 12:55:17 +0900

    Add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_091907) do
+ActiveRecord::Schema.define(version: 2021_07_07_034037) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_091907) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
